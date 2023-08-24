de2c6237e869961dfd45da6589d990ef4c21a065
Balovneva Maria <122821058+Balovneva@users.noreply.github.com>
2023-08-24 21:00:08 +0300

    JRTB-3: added Command pattern for handling Telegram Bot commands (#15)
    * added Command pattern for handling Telegram Bot commands
    
    * updated RELEASE_NOTES.M

---
+++ src/test/java/com/github/javarushcommunity/jrtb/command/UnknownCommandTest.java
@@ -0,0 +1,23 @@
+package com.github.javarushcommunity.jrtb.command;
+
+import org.junit.jupiter.api.DisplayName;
+
+import static com.github.javarushcommunity.jrtb.command.UnknownCommand.UNKNOWN_MESSAGE;
+
+@DisplayName("Unit-level test for UnknownCommand")
+public class UnknownCommandTest extends AbstractCommandTest{
+    @Override
+    String getCommandName() {
+        return "/ffffd";
+    }
+
+    @Override
+    String getCommandMessage() {
+        return UNKNOWN_MESSAGE;
+    }
+
+    @Override
+    Command getCommand() {
+        return new UnknownCommand(sendBotMessageService);
+    }
+}
