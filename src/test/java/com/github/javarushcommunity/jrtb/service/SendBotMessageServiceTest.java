94bdf9cd4d0eba4ce2c88b00aadf4837fc78460f
@mariia_skorobogatova <maria.skor96@gmail.com>
2023-08-24 20:51:04 +0300

    JRTB-3: added Command pattern for handling Telegram Bot commands

---
+++ src/test/java/com/github/javarushcommunity/jrtb/service/SendBotMessageServiceTest.java
@@ -0,0 +1,37 @@
+package com.github.javarushcommunity.jrtb.service;
+
+import com.github.javarushcommunity.jrtb.bot.JavaRushBot;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@DisplayName("Unit-level for SendBotMessageService")
+public class SendBotMessageServiceTest {
+
+    private SendBotMessageService sendBotMessageService;
+    private JavaRushBot javaRushBot;
+
+    @BeforeEach
+    public void init() {
+        javaRushBot = Mockito.mock(JavaRushBot.class);
+        sendBotMessageService = new SendBotMessageServiceImpl(javaRushBot);
+    }
+
+    @Test
+    public void shouldProperlySendMessage() throws TelegramApiException {
+        String chatId = "test_chat_id";
+        String message = "test_message";
+
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setText(message);
+        sendMessage.setChatId(chatId);
+        sendMessage.enableHtml(true);
+
+        sendBotMessageService.sendMessage(chatId, message);
+
+        Mockito.verify(javaRushBot).execute(sendMessage);
+    }
+}
